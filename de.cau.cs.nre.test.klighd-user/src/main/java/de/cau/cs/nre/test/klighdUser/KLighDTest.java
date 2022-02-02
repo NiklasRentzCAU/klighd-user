079e3880cee25560e706c533d6f7c335543c4a6f
Niklas Rentz <nre@informatik.uni-kiel.de>
2022-02-02 11:58:18 +0100

    Working LS build, copying the pragmatics language server into this test project.

---
+++ de.cau.cs.nre.test.klighd-user/src/main/java/de/cau/cs/nre/test/klighdUser/KLighDTest.java
@@ -0,0 +1,45 @@
+///*
+// * KIELER - Kiel Integrated Environment for Layout Eclipse RichClient
+// *
+// * http://rtsys.informatik.uni-kiel.de/kieler
+// * 
+// * Copyright 2022 by
+// * + Kiel University
+// *   + Department of Computer Science
+// *     + Real-Time and Embedded Systems Group
+// * 
+// * This program and the accompanying materials are made available under the
+// * terms of the Eclipse Public License 2.0 which is available at
+// * http://www.eclipse.org/legal/epl-2.0.
+// *
+// * SPDX-License-Identifier: EPL-2.0
+// */
+//
+//package de.cau.cs.nre.test.klighdUser;
+//
+//import de.cau.cs.kieler.klighd.Klighd;
+//import de.cau.cs.kieler.klighd.standalone.KlighdStandaloneSetup;
+//
+///**
+// * Testing some KLighD functionality from command line while only using a plain Maven pom build
+// * 
+// * @author nre
+// */
+//public class KLighDTest {
+//
+//    /**
+//     * @param args
+//     */
+//    public static void main(String[] args) {
+//        try {
+//            System.out.println("Hello World!");
+//            KlighdStandaloneSetup.initialize();
+//            System.out.println(Klighd.IS_PLATFORM_RUNNING);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            throw e;
+//        }
+//
+//    }
+//
+//}
